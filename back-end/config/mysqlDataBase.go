45eb0a5311fefcc891c87fdd3c46d024bb882b7d
Flyinsky2004 <2084151024@qq.com>
2025-01-01 17:35:30 +0800

    1

--- back-end/config/mysqlDataBase.go
+++ back-end/config/mysqlDataBase.go
@@ -16,8 +16,8 @@ import (
 var MysqlDataBase *gorm.DB
 
 func InitMysqlDataBase() {
-	//dbc := "root:root@tcp(127.0.0.1:9998)/ideacosmos?charset=utf8mb4&parseTime=True&loc=Local"
-	dbc := "root:Wjywjy2333@@tcp(127.0.0.1:3306)/ideacosmos?charset=utf8mb4&parseTime=True&loc=Local"
+	dbc := "root:root@tcp(127.0.0.1:9998)/ideacosmos?charset=utf8mb4&parseTime=True&loc=Local"
+	//dbc := "root:Wjywjy2333@@tcp(127.0.0.1:3306)/ideacosmos?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dbc), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("连接数据库时发生错误:%v", err)
